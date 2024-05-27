e7b6a1179670a8e75ee318b0990085268e9cf3b0
Donato Carola <don94@live.it>
2024-05-27 16:06:05 +0200

    first commit

---
+++ main.java
@@ -0,0 +1,25 @@
+//! esercizio 1
+//Scrivere un programma Java per invertire un array di valori interi
+
+// public class main {
+
+//     public static void main(String[] args) {
+//         int[] array = { 20, 10, 5, 4, 3, 2, 1 };
+
+//         int index = array.length-1;                       //salviamo in una variabile l'ultimo elemento dell'array
+
+//         for (int i = 0; i <= array.length / 2; i++) {
+//             int tmp = array[i];        //variabile temporanea per salvare il contenuto di array[i]
+//             array[i]= array[index];
+//             array[index] = tmp;
+//             index--;
+//         }
+
+//         for(int k = 0; k<array.length; k++){
+//             System.out.println(array[k]);
+//         }
+
+//     }
+// }
+
+
